baa902fc6bedef93437b11def6ea0a97cf7d1e69
JhoverAMulluniJove <108031112+JhoverAMulluniJove@users.noreply.github.com>
2022-08-11 15:20:04 -0500

    Add files via upload

---
+++ ED_Estructura/lib/lstCirSimpLig/index.js
@@ -0,0 +1,242 @@
+import {ListaCircular} from './lista.js';
+import {bootbox_prompt,bootbox_alert} from '../utils/dialog.js';
+
+let lista=null;
+
+export async function insertar_al_inicio(){
+    var canvas=document.getElementById('tutorial'); 
+    if(lista==null){
+        lista=new ListaCircular(canvas);		
+    }
+    var DATO = await bootbox_prompt("Ingrese valor de nodo");
+    if(DATO==null || DATO.trim()==""){
+        return;
+    }
+    try{
+        DATO=DATO.trim();
+        if(lista.buscar1(DATO)==true){
+            throw new Error("EL NODO YA SE INGRESO");
+        }
+        lista.inserta_inicio(DATO);
+        lista.dibujarNodosLog();	
+        lista.dibujarNodos(DATO);
+    }catch(e){
+        await bootbox_alert(e.message);
+    }
+}////////////////////////////////////////////////////////
+export async function insertar_al_final(){
+    var canvas=document.getElementById('tutorial'); 
+    if(lista==null){
+        lista=new ListaCircular(canvas);		
+    }
+    
+    try{
+        if(lista.isVacio()==true){//la lista esta vacia
+            throw new Error("LA LISTA ESTA VACIA");
+        }
+
+        var DATO = await bootbox_prompt("Ingrese valor de nodo");
+        if(DATO==null || DATO.trim()=="")
+            return;
+
+        DATO=DATO.trim();
+        if(lista.buscar1(DATO)==true)
+                throw new Error("EL NODO YA SE INGRESO");
+
+        lista.inserta_final(DATO);
+        lista.dibujarNodosLog();	
+        lista.dibujarNodos(DATO);
+    
+    }catch(e){
+       await bootbox_alert(e.message);
+    }
+
+}//////////////////////////////////////////////////////////
+export async function insertar_antes_x(){
+    var canvas=document.getElementById('tutorial'); 
+    if(lista==null){
+        lista=new ListaCircular(canvas);		
+    }
+    
+    try{
+
+        if(lista.isVacio()==true){//la lista esta vacia
+            throw new Error("LA LISTA ESTA VACIA");
+        }
+        
+        var DATO = await bootbox_prompt("Ingrese valor de DATO");
+        if(DATO==null || DATO.trim()=="")
+                return;
+        DATO=DATO.trim();
+        if(lista.buscar1(DATO)==true)
+                throw new Error("EL NODO YA SE INGRESO");
+
+        var X = await bootbox_prompt("ingrese valor de X");
+        if(X==null || DATO.trim()=="")
+                return;
+        if(lista.buscar1(X)==false)
+                throw new Error("EL NODO EXISTE");
+
+        X=X.trim();
+        lista.inserta_antes_X(DATO,X);
+        lista.dibujarNodosLog();	
+        lista.dibujarNodos(DATO);
+        
+    }catch(e){
+        await bootbox_alert(e.message);
+    }
+
+}/////////////////////////////////////////////////////////
+export async function insertar_despues_x(){
+    var canvas=document.getElementById('tutorial'); 
+    if(lista==null){
+        lista=new ListaCircular(canvas);		
+    }
+    
+    try{
+
+        if(lista.isVacio()==true){//la lista esta vacia
+            throw new Error("LA LISTA ESTA VACIA");
+        }
+        
+        var DATO = await bootbox_prompt("Ingrese valor de DATO");
+        if(DATO==null || DATO.trim()=="")
+                return;
+
+        DATO=DATO.trim();
+        if(lista.buscar1(DATO)==true)
+                throw new Error("EL NODO YA SE INGRESO");
+
+        var X = await bootbox_prompt("ingrese valor de X");
+        if(X==null || DATO.trim()=="")
+                return;
+        if(lista.buscar1(X)==false)
+                throw new Error("EL NODO EXISTE");
+        
+        X=X.trim();
+        lista.inserta_despues_X(DATO,X);
+        lista.dibujarNodosLog();	
+        lista.dibujarNodos(DATO);
+        
+    }catch(e){
+        await bootbox_alert(e.message);
+    }
+
+}/////////////////////////////////////////////////////////
+export async function eliminar_inicio(){
+    var canvas=document.getElementById('tutorial'); 
+    if(lista==null){
+        lista=new ListaCircular(canvas);		
+    }
+
+    try{
+        if(lista.isVacio()==true){//la lista esta vacia
+            throw new Error("LA LISTA ESTA VACIA");
+        }
+
+        await lista.elimina_inicio();
+        lista.dibujarNodosLog();	
+        lista.dibujarNodos();
+
+        await bootbox_alert("NODO ELIMINADOS SATISFACTORIAMENTE");
+    
+    }catch(e){
+        await bootbox_alert(e.message);
+    }
+}/////////////////////////////////////////////////////////
+export async function eliminar_final(){
+    var canvas=document.getElementById('tutorial'); 
+    if(lista==null){
+        lista=new ListaCircular(canvas);		
+    }
+
+    try{
+        if(lista.isVacio()==true){//la lista esta vacia
+            throw new Error("LA LISTA ESTA VACIA");
+        }
+
+        await lista.elimina_final();
+        lista.dibujarNodosLog();	
+        lista.dibujarNodos();
+
+        await bootbox_alert("NODO ELIMINADOS SATISFACTORIAMENTE");
+    
+    }catch(e){
+        await bootbox_alert(e.message);
+    }
+}/////////////////////////////////////////////////////////
+export async function eliminar_nodo_x(){
+    var canvas=document.getElementById('tutorial'); 
+    if(lista==null){
+        lista=new ListaCircular(canvas);		
+    }
+
+    try{
+        if(lista.isVacio()==true){//la lista esta vacia
+            throw new Error("LA LISTA ESTA VACIA");
+        }
+        var DATO = await bootbox_prompt("Ingrese valor del nodo X");
+        if(DATO==null)
+                return;
+        if(lista.buscar1(DATO)==false)
+                throw new Error("EL NODO NO EXISTE");
+        await lista.elimina_nodo_X(DATO);
+        lista.dibujarNodosLog();	
+        lista.dibujarNodos();
+
+        await bootbox_alert("NODO ELIMINADOS SATISFACTORIAMENTE");
+    
+    }catch(e){
+        await bootbox_alert(e.message);
+    }
+}/////////////////////////////////////////////////////////
+export async function eliminar_antes_x(){
+    var canvas=document.getElementById('tutorial'); 
+    if(lista==null){
+        lista=new ListaCircular(canvas);		
+    }
+
+    try{
+        if(lista.isVacio()==true){//la lista esta vacia
+            throw new Error("LA LISTA ESTA VACIA");
+        }
+        var DATO = await bootbox_prompt("Ingrese valor del nodo X");
+        if(DATO==null)
+                return;
+        if(lista.buscar1(DATO)==false)
+                throw new Error("EL NODO NO EXISTE");
+        await lista.elimina_antes_X(DATO);
+        lista.dibujarNodosLog();	
+        lista.dibujarNodos();
+
+        await bootbox_alert("NODO ELIMINADOS SATISFACTORIAMENTE");
+    
+    }catch(e){
+        await bootbox_alert(e.message);
+    }
+}/////////////////////////////////////////////////////////
+export async function eliminar_despues_x(){
+    var canvas=document.getElementById('tutorial'); 
+    if(lista==null){
+        lista=new ListaCircular(canvas);		
+    }
+
+    try{
+        if(lista.isVacio()==true){//la lista esta vacia
+            throw new Error("LA LISTA ESTA VACIA");
+        }
+        var DATO = await bootbox_prompt("Ingrese valor del nodo X");
+        if(DATO==null)
+                return;
+        if(lista.buscar1(DATO)==false)
+                throw new Error("EL NODO NO EXISTE");
+        await lista.elimina_despues_X(DATO);
+        lista.dibujarNodosLog();	
+        lista.dibujarNodos();
+
+        await bootbox_alert("NODO ELIMINADOS SATISFACTORIAMENTE");
+    
+    }catch(e){
+        await bootbox_alert(e.message);
+    }
+}/////////////////////////////////////////////////////////
